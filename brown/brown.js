6ad24535dc3a2050dfe46ea4e743d3bc480d924c
alejandronaranjocaraza <216925605+alejandronaranjocaraza@users.noreply.github.com>
2025-09-02 12:06:25 -0600

    Adds sketch template

--- brown/brown.js
+++ brown/brown.js
@@ -1,18 +1,40 @@
 const conts = [];
 
-for (let i = 0; i<4; i++) {
-  conts[i] = document.createElement("div");
-  conts[i].setAttribute("id","div".concat(i))
-  conts[i].setAttribute("class","container")
-  grid = document.querySelector(".grid")
-  grid.appendChild(conts[i])
-  
-  for (let j = 0; j<4; j++) {
-    let e = document.createElement("div");
-    e.setAttribute("id","element".concat(i,j))
-    e.setAttribute("class","element")
-    conts[i].appendChild(e);
 
+const btn = document.querySelector("button");
+const inpt = document.querySelector("input");
+btn.addEventListener("click",() => generateGrid(inpt));
+
+
+
+function lightUp(e){
+  e.target.setAttribute("style","background: red;");
+}
+
+function generateGrid(inp) {
+  var n = inp.value;
+  if (n>100 || parseInt(n)!=n) {
+    console.log("Invalid input");
+    console.log(typeof(n));
+    return;
+  }
+  grid = document.querySelector(".grid");
+  grid.innerHTML = "";
+  for (let i = 0; i<n; i++) {
+    conts[i] = document.createElement("div");
+    conts[i].setAttribute("id","div".concat(i))
+    conts[i].setAttribute("class","container");
+    grid.appendChild(conts[i]);
+    
+    for (let j = 0; j<n; j++) {
+      let e = document.createElement("div");
+      e.setAttribute("id","element".concat(i,j));
+      e.setAttribute("class","element");
+      e.setAttribute("style","background: white;");
+      e.addEventListener("mouseover", (e) => lightUp(e));
+      conts[i].appendChild(e);
+
+    }
   }
 }
 
